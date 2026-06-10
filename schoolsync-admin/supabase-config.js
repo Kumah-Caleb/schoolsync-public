1f47260ac46dd2ccd5da61abb6b9f4faaec242b3
Kumah Caleb <kumahmensahcaleb@gmail.com>
2026-06-10 01:07:27 +0100

    Fixed Supabase URL from .com to .co

--- schoolsync-admin/supabase-config.js
+++ schoolsync-admin/supabase-config.js
@@ -1,5 +1,4 @@
 // supabase-config.js
-const SUPABASE_URL = 'https://kuqtkkehnzxapyophock.supabase.com'  // Your actual Project URL
-const SUPABASE_KEY = 'sb_publishable_zethfzTxF9Q9FFdh46dDxA_EESc72eg
-'  // Your publishable key
+const SUPABASE_URL = 'https://kuqtkkehnzxapyophock.supabase.co'
+const SUPABASE_KEY = 'sb_publishable_zethfzTxF9Q9FFdh46dDxA_EESc72eg'
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
